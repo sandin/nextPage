ff304648248049217141ecbfa765b31d2cc2d6fa
lds@win7 <lds2012@gmail.com>
2010-09-05 01:11:27 +0800

    first version

--- src/FindNextPage.js
+++ src/FindNextPage.js
@@ -1,121 +1,221 @@
-//console.log('start');
-(function(){
+/** 
+ * @file FindNextPage.js
+ * 
+ * @author lds <lds2012@gmail.com>
+ * @version %Id
+ * @date 2010-09-04
+ */
+(function(window, undefined){
 
 
-// Namespace
-window.Lds = {}
+/** @namespace */
+window.Lds = {};
+
+// shortcuts
+var document = window.document;
 
 
 var FindNextPage = { 
 
-    // version
+    /** 
+     * Current version
+     *
+     * @type string
+     */
     version : '1.0',
 
-    // only for debuger
-    DEBUG  : true,
+    /**
+     * Debug or not, it's only for debug
+     *
+     * @type bool
+     */
+    DEBUG  : false,
 
-    // search pattern
+    /**
+     * How many link matched for debug.
+     *
+     * @type number
+     * @private
+     */
+    _matched : 0,
+
+    /**
+     * Search pattern
+     *
+     * @type string
+     * @private
+     */
     _grep : /Next|下一页|下页|后页|^>>$/ig,
 
-    // count press time
+    /**
+     * Count press time
+     *
+     * @type number
+     * @private
+     */
     _count : 0,
 
-    // next page URL
+    /**
+     * Next page URL
+     *
+     * @type string
+     */
     url : '',
 
-    // keyCode Map
+    /**
+     * keyCode Map
+     *
+     * @type object
+     */
     keyCode : {
         // ']'
         bracket : 221
     },
 
+    /**
+     * Init 
+     *
+     * @returns {bool} 
+     */
     init : function () { 
-        // bind All the event handler
-        this.bindEvent();
         // save it to the namespace
         window.Lds.FindNextPage = this;
+
+        // bind All the event handler
+        this.bindEvent();
+
+        return true;
     },
 
+    /** 
+     * Set search pattern
+     *
+     * @param {RepExp} grep
+     * 
+     * @returns {bool} Return false when param is not a RepExp Object
+     */
     setGrep : function (grep) {
         // grep must be a RepExp Object
         if ('undefined' !== typeof grep && !!grep && grep instanceof RegExp) {
             this._grep = grep;
+            return true;
         }
         else {
             return false;
         }
     },
 
+    /**
+     * Bind event handler  
+     *
+     * @returns {bool} Always return true
+     */
     bindEvent : function () {
         // shortcut
         var self  = this;
 
         // Bind document keydown event
-        document.addEventListener('keydown', function(e){
-            //if (this.DEBUG) console.log(e.keyCode);
+        document.addEventListener('keyup', function(e){
+            if (self.DEBUG) { 
+                console.log('keyCode : ' + e.keyCode + ' & count : '   + self._count);
+            }
 
-            // Double press ']' then redirect to the next page
-            if(self.keyCode.bracket != e.keyCode && self._count > 0)
+            // press ']' first and then press another key 
+            if (self.keyCode.bracket != e.keyCode && self._count > 0) {
                 self._count = 0;
+            }
 
-            if(self.keyCode.bracket === e.keyCode) {
-                if (0 == self._count) {
-                    self._count++; // press ']' at first time
-                }
-                else if (1 == self._count) { 
+            if (self.keyCode.bracket === e.keyCode) {
+                if ( 1 === self._count ) {
+                    self._count = 0; // reset count
                     self.goToNextPage(); // double press ']'
                 }
                 else {
-                    self._count = 0; // reset
+                    self._count++; // press ']' at first time
                 }
             }
-        }, true);
+        }, false);
+
+        return true;
     },
 
+    /**
+     * goToNextPage
+     *
+     * This function will redirect to the next page 
+     * when find the 'next page' link in this page HTML.
+     *
+     * @returns void
+     *  
+     */
     goToNextPage : function () {
         // all links , TAG <a>
         var links = document.getElementsByTagName('a');
 
         // has no link
-        if (0 === links.lenght) return false; 
+        if (0 === links.length) return false; 
 
         for (a in links) {
-            if (!! links[a].innerHTML && -1 != links[a].innerHTML.search(this._grep) ) {
-
-                //console.log('FIND', links[a], links[a].getAttribute('href') );
+            if ( links[a].innerHTML && -1 != links[a].innerHTML.search(this._grep) ) {
 
                 // Next page URL
                 this.url = links[a].getAttribute('href');
                 this.url = this.parserURL(this.url);
 
                 if ( this.DEBUG ) {
-                    console.log("location.href = " + this.url);
+                    console.log("location.href : " + 
+                                this.url + ' & HTML : ' +
+                                links[a].innerHTML);
+                    this._matched++;
                 } else {
                     // redirect to next page
                     location.href = this.url;
+                    break;
                 }
-
-                break;
             }
         }
     },
 
+    /**
+     * parserURL Parser URL which is the next page link
+     *
+     * @param {string} url
+     *
+     * @returns {string} url 
+     */
     parserURL : function (url) {
-
-        // HANDLE: start with '/', LIKE:'/path/to'
-        if ('/' === url.charAt(url.length -1)) 
-            url = url.slice(0, -1);
-
-        // HANDLE: not start with 'http://'
-        if (0 != url.indexOf('http://')) {
-            url =  'http://' + document.domain + '/' +  url;
-        }
-
+        // nothing to do
         return url;
+    },
+
+    /**
+     * destroy 
+     *
+     * @returns void
+     */
+    destroy : function () {
+        this._matched = 0;
+        this._conut = 0;
     }
 }
 
-// when document ready then init it 
+// When document ready, init it 
 FindNextPage.init();
 
-})();
+// communicate with this extension background page
+chrome.extension.sendRequest({command: "getOptions"}, function(options) {
+
+    // set search pattern with user options
+    if ( 'undefined' !== typeof options.grep &&
+         'string' === typeof options.grep) {
+        var pattern = new RegExp(options.grep,'i');
+        FindNextPage.setGrep(pattern);
+
+        if (FindNextPage.DEBUG)
+            console.log('search pattern : ' + FindNextPage._grep);
+    }
+});
+
+})(window);
+
+// vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 enc=utf-8 :
--- tests/FindNextPage_test.js
+++ tests/FindNextPage_test.js
@@ -1,18 +1,56 @@
+/** 
+ * @file FindNextPage_test.js
+ * 
+ * @author lds <lds2012@gmail.com>
+ * @version %Id
+ * @date 2010-09-04
+ */
+
+
+// shortcut
 if (window.console && window.console.log) {
     log = window.console.log;
 } else {
     log = window.alert;
 }
 
-var obj;
+var obj, // FindNextPage object
+    linksNumber, // How many links
+    isNextPageNumber; // How many are next page links
 
-module("init");
+module('init this test');
+
+test('init this test', function () {
+
+    // test date
+    var HTML = '<div id="testZone" style="display:none">' +
+    '<a href="notNextPage.html">链接</a>' +
+    '<a href="notNextPage.html">文字</a>' +
+    '<a href="notNextPage.html">链接</a>' +
+    '<a href="isNextPage.html">下一页</a>' +
+    '<a href="isNextPage.html"><span>下一页</span></a>' +
+    '<a href="isNextPage.html">下页</a>' +
+    '<a href="isNextPage.html">next</a>' +
+    '<a href="isNextPage.html">Next</a>' +
+    '<a href="notNextPage.html">链接</a>' +
+    '</div>';
+
+    jQuery('body').append(HTML);
+    ok( 1 === jQuery('#testZone').length, 'test date is already' );
+
+    var $links = jQuery('#testZone>a');
+
+    linksNumber = $links.length;
+    isNextPageNumber = $links.filter('[href="isNextPage.html"]').length;
 
 
+});
+
+// tests all methods
 module("methods");
 
 // init method
-test('init', function () {
+test('test init', function () {
 
     ok(window.Lds && 
        window.Lds.FindNextPage &&
@@ -27,11 +65,12 @@ test('init', function () {
 });
 
 // setGrep method
-test('setGrep', function(){
+test('test setGrep', function(){
 
     // test String, Number, Array, Object,
     // and RepExp
-    var myDate = [ 'string', 23, {}, [], /RepExp/ ];
+    var myDate = [ 'string', 23, {}, [], /RepExp/ ],
+        oldGrep = obj._grep; // save old grep
 
     // only accept RepExp Object
     for (i in myDate) {
@@ -44,16 +83,21 @@ test('setGrep', function(){
             ok(! result, 'setGrep unaccept : ' + type );
         }
     }
+
+    // resume
+    obj.setGrep(oldGrep);
 });
 
 // bindEvent method
-test('bindEvent', function () {
-    log(Lds);
-    //var e = jQuery.event;
-    //e.keyCode = 221;
-    //jQuery('body').trigger('keyDown');
-    //jQuery('body').trigger('keyDown');
+test('test bindEvent', function () {
 
-
+    // simulate press the hotkey
+    // double press ']'
+    jQuery(document).simulate('keyup', { keyCode: obj.keyCode.bracket });
+    jQuery(document).simulate('keyup', { keyCode: obj.keyCode.bracket });
+    
+    same(obj._matched, isNextPageNumber, 'All matched');
 });
 
+
+// vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 enc=utf-8 :
